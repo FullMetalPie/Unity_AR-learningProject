fcebf2cb918f11c0bc2eae9a9bc76d030e38a6ad
FullMetalPie <andrea.pietri06@gmail.com>
2025-02-19 11:18:36 +0100

    Fixed(?) compilation errors due to merge

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -13,19 +13,3 @@ public class MainMenu : MonoBehaviour
     }
 
 }
-=======
-using UnityEngine;
-using UnityEngine.SceneManagement;
-public class MainMenu : MonoBehaviour
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    public void playGame() {
-        SceneManager.LoadScene("AR_ImageTracking");
-    }
-
-    public void gitHubButton() {
-        Application.OpenURL("https://github.com/FullMetalPie");
-    }
-
-}
->>>>>>> 6f2382a796c301a5a5907829de6846f59153c7cb
--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -10,16 +10,3 @@ public class Rotator : MonoBehaviour
         transform.Rotate (new Vector3 (0, 15, 0) * Time.deltaTime);
     }
 }
-=======
-using UnityEngine;
-
-public class Rotator : MonoBehaviour
-{
-
-    // Update is called once per frame
-    void Update()
-    {
-        transform.Rotate (new Vector3 (0, 15, 0) * Time.deltaTime);
-    }
-}
->>>>>>> 6f2382a796c301a5a5907829de6846f59153c7cb
