00d9fecc0503121c5e24c7d8df6b996734c35902
FullMetalPie <andrea.pietri06@gmail.com>
2025-02-19 12:49:15 +0100

    Implemented navigation between scenes (TO FIX)

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -4,7 +4,7 @@ public class MainMenu : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public void playGame() {
-        SceneManager.LoadScene("AR_ImageTracking");
+        SceneManager.LoadSceneAsync("AR_ImageTracking");
     }
 
     public void gitHubButton() {
